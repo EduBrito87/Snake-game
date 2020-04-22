4a456aeac8433d4ef4bb1945cabe8c79e4c5557c
Eduardo Silva <edubrito87@gmail.com>
2020-04-22 13:08:34 -0300

    definir fim do Jogo

--- script.js
+++ script.js
@@ -38,12 +38,20 @@ function update (event){
     if (event.keyCode == 40 && direction != "up") direction = "down"
 }
 
+
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(let i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].yy == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :( ');
+        }
+    }
 
     criarBG();
     criarCobrinha();
